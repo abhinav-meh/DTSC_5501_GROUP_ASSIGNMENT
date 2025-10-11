3635301445b1e13afd51b54430a43f3e9f688c3b
eenoonan <eenoonan00@gmail.com>
2025-10-11 15:49:46 -0600

    Update conflictCheck.py

--- src/conflictCheck.py
+++ src/conflictCheck.py
@@ -1,14 +1,15 @@
-def checkConflicts(event_id, time_stamp, location):
+def checkConflicts(event_id, time_stamp, location, return_list):
     start_time = time.time()
 
     checked = []
     conflicts = []
+    conflict_id = []
 
     con_instance = 0
     conflict_total = 0
     
     for j in range(event_id[-1]):
-        #print(j)
+        
         chk_sked = time_stamp[j]
         chk_loc = location[j]
         
@@ -26,6 +27,11 @@ def checkConflicts(event_id, time_stamp, location):
             for k in range(j+1,event_id[-1]):
                 if(time_stamp[k] == chk_sked and location[k] == chk_loc):
                     conflict_count +=1
+                    if(conflict_count == 1):
+                        conflict_id.append(j+1)
+                        conflict_id.append(k+1)
+                    else:
+                        conflict_id.append(k+1)
                     #print(f"conflict identified - count {conflict_count}")
 
             if(k == event_id[-2]) and conflict_count > 0:
@@ -37,8 +43,9 @@ def checkConflicts(event_id, time_stamp, location):
 
     print(f"{conflict_total} conflicts identified for {con_instance} time / loc combinations")
 
-    #print(f"Conflict List: {conflicts}")
-
     print(f"Conflict checking time: {conflict_chk_time} s")
     
-    return conflicts, conflict_chk_time
+    if(return_list == 1):
+        return conflict_total, con_instance, conflict_chk_time, conflict_id
+    else:
+        return conflict_total, con_instance, conflict_chk_time
