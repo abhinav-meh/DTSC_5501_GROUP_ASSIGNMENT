0ed544e5c00052c5394cef1a5ba4f04c43dabfec
eenoonan <eenoonan00@gmail.com>
2025-10-12 14:50:39 -0600

    Update checkConflicts.py

--- src/checkConflicts.py
+++ src/checkConflicts.py
@@ -8,12 +8,14 @@ from src.ArrayListEventStore import ArrayListEventStore
 def checkConflicts(array, return_list):
     # array is array/list to be searched for conflicts
     # return list is 1 to print a list of conflicts, 0 to skip printing (for large data sets)
-    start_time = time.time()
+    start_time = time.time() # start execution time
 
+    # set variables to collect data
     checked = [] 
     conflicts = []
     conflict_id = []
 
+    # zero counters
     con_instance = 0
     conflict_total = 0
 
@@ -21,36 +23,45 @@ def checkConflicts(array, return_list):
     
     for j in range(size):
         #print(j)
-        chk_sked = array.getTime()[j] #first 
-        chk_loc = array.getLocation()[j]
+        # Event parameters to check for conflicts
+        chk_sked = array.getTime()[j] # timestamp
+        chk_loc = array.getLocation()[j] # location
         
-        chk_combo = chk_loc + '_' + str(chk_sked)
+        chk_combo = chk_loc + '_' + str(chk_sked) # date / time / location combined
 
+        # check if date / time / location combination has been checked for conflicts already
+        # if so, skip to next combination
         if(chk_combo in checked):
             #print(f"{chk_combo} already checked")
             continue
+        # if date / time / location combination has not been checked for conflicts, continue
         else:
-            checked.append(chk_combo)
+            checked.append(chk_combo) # note combination being checked 
             #print(f"checking {chk_combo}")
 
-            conflict_count = 0
-            
-            for k in range(j+1,size):#event_id[-1]):
+            conflict_count = 0 # zero conflict count
+
+            # comparison starts with next event after event being checked
+            for k in range(j+1,size):
+                # if match of timestamp and location
                 if(array.getTimestamp()[k] == chk_sked and array.getLocation()[k] == chk_loc):
                     conflict_count +=1
+                    # if first conflict instance, note both event ids
                     if(conflict_count == 1):
                         conflict_id.append(event_id[j])
                         conflict_id.append(event_id[k])
+                    # if subsequent conflict instance, note only new event id
                     else:
                         conflict_id.append(event_id[k])
                     #print(f"conflict identified - count {conflict_count}")
-
-            if(k == (size-1)) and conflict_count > 0: #k == event_id[-2]
+            # if reach the end of the search and conflicts have been identified
+            # document conflict counts and details
+            if(k == (size-1)) and conflict_count > 0: 
                 conflicts.append([chk_combo, conflict_count])
                 con_instance += 1
                 conflict_total += conflict_count
-    end_time = time.time()
-    conflict_chk_time = end_time - start_time
+    end_time = time.time() # stop execution time
+    conflict_chk_time = end_time - start_time # calculate run time
 
     print(f"{conflict_total} conflicts identified for {con_instance} time / loc combinations")
 
@@ -60,4 +71,4 @@ def checkConflicts(array, return_list):
     if(return_list == 1):
         return conflict_total, con_instance, conflict_chk_time, conflict_id
     else:
-        return conflict_total, con_instance, conflict_chk_time  
+        return conflict_total, con_instance, conflict_chk_time      
