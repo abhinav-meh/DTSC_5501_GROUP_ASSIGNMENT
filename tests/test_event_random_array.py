c8f2badbbbb2c618b4cdba00452fb4cc2d2e6214
eenoonan <eenoonan00@gmail.com>
2025-10-14 20:21:41 -0600

    Create test_event_random_array.py

---
+++ tests/test_event_random_array.py
@@ -0,0 +1,8 @@
+def test_event_random_array(x):
+    EventList = ArrayListEventStore(x)
+    for i in range(x):           
+        eventi = Event_Random(i+1)#, f"Event {i}", date, time_start, location)
+        if(x<15):print(f"Event generated {eventi}")
+        EventList.insert_event(eventi)
+        
+    return EventList    
