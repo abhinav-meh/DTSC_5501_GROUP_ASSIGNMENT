1e040c91e9b2ccc2392801a6782c9877e303e858
eenoonan <eenoonan00@gmail.com>
2025-10-14 20:46:42 -0600

    Create test_event_fixed_linked.py

---
+++ tests/test_event_fixed_linked.py
@@ -0,0 +1,25 @@
+from LinkedListEventStore import LinkedListEventStore
+from EventStore import EventStore
+from Event import Event
+from Node import Node
+
+def test_event_fixed_linked(x):
+    import random
+    EventList = LinkedListEventStore()
+    for i in range(x): 
+        if (i//2 !=0 and i < 4):
+            eventi = Event(i+1, f"Event {i+1}", "2026-01-01", "10:00", "Conf Rm 42")
+        else:
+            s = random.randint(1,x)
+            if(s//2 == 0 and s>5):
+                eventi = Event(i+1, f"Event {i+1}", "2026-07-13", "15:00", "Bldg 2")
+            elif(s == 5): 
+                eventi = Event(i+1, f"Event {i+1}", "2026-04-19", "18:00", "Bldg 2")
+            elif(s//3 ==0): 
+                eventi = Event(i+1, f"Event {i+1}", "2026-12-06", "06:00", "Bldg 1")
+            else:
+                eventi = Event(i+1, f"Event {i+1}", "2026-12-06", "10:00", "Bldg 2")
+        print(f"Event generated {eventi}")
+        EventList.insert_event(eventi)
+        
+    return EventList    
