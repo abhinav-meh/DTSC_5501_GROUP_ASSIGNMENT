0637fe3bfc7a0416c056f747e3ba61f5352a0895
eenoonan <eenoonan00@gmail.com>
2025-10-15 18:56:13 -0600

    Create test_mult_cases_linked.py

---
+++ tests/test_mult_cases_linked.py
@@ -0,0 +1,27 @@
+from testRandomDataLinked import testRandomDataLinked
+
+def test_mult_cases_linked(test_inc):
+    ind = 0
+
+    n = len(test_inc)
+
+    conflict_count_tot = [0] * n
+    con_instances = [0] * n
+    conflict_chk_time = [0] * n
+    conflict_id_list = [0] * n
+
+    for m in range(n):
+        z = test_inc[m]
+        print(f"checking {z} events")
+        #print(z)
+
+        conflict_count_tot[m], con_instances[m], conflict_chk_time[m] = testRandomDataLinked(z,0)
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(test_inc, conflict_chk_time, marker='o', linestyle='-', color='blue', label='?')
+    plt.xlabel("Events Checked")
+    plt.ylabel("Time (s)")
+    plt.title("Time to Execute Conflict Checking for Linked List")
+    plt.grid(True)
+    plt.show()
