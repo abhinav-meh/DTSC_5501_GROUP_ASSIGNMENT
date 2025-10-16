494cbae052df7a9ce0daab8c95c298970147f368
eenoonan <eenoonan00@gmail.com>
2025-10-15 19:07:07 -0600

    Update test_event_random_array.py

--- tests/test_event_random_array.py
+++ tests/test_event_random_array.py
@@ -1,11 +1,11 @@
 from ArrayListEventStore import ArrayListEventStore
 from Event_Random import Event_Random
 
-def test_event_random_array(x):
-    EventList = ArrayListEventStore(x)
-    for i in range(x):           
+def test_event_random_array(event_count):
+    EventList = ArrayListEventStore(event_count)
+    for i in range(event_count):           
         eventi = Event_Random(i+1)#, f"Event {i}", date, time_start, location)
-        if(x<15):print(f"Event generated {eventi}")
+        if(event_count<15):print(f"Event generated {eventi}")
         EventList.insert_event(eventi)
         
     return EventList    
