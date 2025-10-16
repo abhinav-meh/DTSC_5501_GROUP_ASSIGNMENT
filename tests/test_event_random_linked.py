9fb23dfc028de163146aa1d9c81bba68a9b87b6c
eenoonan <eenoonan00@gmail.com>
2025-10-15 19:06:28 -0600

    Update test_event_random_linked.py

--- tests/test_event_random_linked.py
+++ tests/test_event_random_linked.py
@@ -4,11 +4,11 @@ from Event_Random import Event_Random
 from Node import Node
 
 
-def test_event_random_linked(x):
+def test_event_random_linked(event_count):
     EventList = LinkedListEventStore()
-    for i in range(x):           
+    for i in range(event_count):           
         eventi = Event_Random(i+1)#, f"Event {i}", date, time_start, location)
-        if(x<15):print(f"Event generated {eventi}")
+        if(event_count<15):print(f"Event generated {eventi}")
         EventList.insert_event(eventi)
         
     return EventList    
